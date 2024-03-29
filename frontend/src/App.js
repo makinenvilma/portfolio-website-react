c557b02a3871e9d67201f6fa847285095d1a3138
Vilma <vilmakinen@outlook.com>
2024-03-29 15:03:11 +0200

    Fix: stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Welcome to My React App</h1>
       </header>
+      <main>
+        <p>This is a test.</p>
+      </main>
+      <footer>
+        <p>&copy; 2024 My Portfolio</p>
+      </footer>
     </div>
   );
 }
