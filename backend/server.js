ffb560987e7b51af11d6f5be43e11adc30fb66fc
Vilma <vilmakinen@outlook.com>
2024-03-30 20:12:21 +0200

    Add: docker stuff

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const {Pool} = require('pg');
+const dotenv = require('dotenv');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('This is a test.');
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.get('/', async (req, res) => {
+  try {
+    // Example query to select data from a "projects" table
+    const {rows} = await pool.query('SELECT * FROM projects');
+    res.json(rows); // Send retrieved data as JSON response
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 const PORT = process.env.PORT || 5000;
