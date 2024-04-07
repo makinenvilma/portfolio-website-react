94753f3c58509ebb6d9a5c0aa1ce1b23dcca4da2
Vilma <vilmakinen@outlook.com>
2024-04-07 18:26:35 +0300

    Add: helmet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/navigation/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Helmet} from 'react-helmet';
 import Home from './pages';
 import About from './pages/about';
 import Projects from './pages/projects';
@@ -9,6 +10,14 @@ import Contact from './pages/contact';
 function App() {
   return (
     <Router>
+      <Helmet>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'
+        />
+      </Helmet>
       <Navbar /> {/* Render Navbar component */}
       <Routes>
         <Route exact path='/' element={<Home />} />
