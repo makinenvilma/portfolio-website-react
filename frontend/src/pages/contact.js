4a43140322e62df36471570c443d4951e54ab427
Vilma <vilmakinen@outlook.com>
2024-04-12 12:22:25 +0300

    Add: contact page

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import '../App.css';
+import './contact.css';
 
 const Contact = () => {
   return (
     <div>
-      <header>
-        <h1>Contact Me</h1>
-      </header>
+      <header></header>
       <main>
-        <p>This is a test.</p>
+        <div class='contact-title'>
+          <h2>Contact Me</h2>
+          <p>This is a test.</p>
+        </div>
+        <div class='contact-box'>
+          <div class='contact-flex-container'>
+            <div class='contact-flex-container-text'>
+              <h3>Linkedin</h3>
+              <p>This is a test number one.</p>
+              <div class='contact-flex-container-link'>
+                <p>Test1</p>
+              </div>
+            </div>
+            <div class='contact-flex-container-text'>
+              <h3>Github</h3>
+              <p>This is a test number two.</p>
+              <div class='contact-flex-container-link'>
+                <p>Test1</p>
+              </div>
+            </div>
+            <div class='contact-flex-container-text'>
+              <h3>Email</h3>
+              <p>This is a test number three.</p>
+              <div class='contact-flex-container-link'>
+                <p>Test1</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
