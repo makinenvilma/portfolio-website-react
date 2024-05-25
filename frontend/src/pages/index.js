85f8ca154239bf67b335f0ddfcf14e3fa6d85b7c
Vilma <vilmakinen@outlook.com>
2024-05-25 11:49:17 +0300

    Add: assets folder

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import index_image from '../assets/images/index_image.png';
 
 const Home = () => {
   return (
@@ -9,10 +10,7 @@ const Home = () => {
         <div class='info-box'>
           <div class='info-flex-container'>
             <div class='info-flex-container-image'>
-              <img
-                src='https://cdn.discordapp.com/attachments/902806118156357653/1224452741137432727/vikitteleva_koira-removebg-preview_1.png?ex=661d8b7a&is=660b167a&hm=c5566d3d260a5da04e618b8b7a1ebcbc3b4e16f50c47c1f3063f7a6d9a4cf8de&'
-                alt='Example'
-              ></img>
+              <img src={index_image} alt='Me' />
             </div>
             <div class='info-flex-container-text'>
               <h1>A Software Developer</h1>
@@ -25,7 +23,7 @@ const Home = () => {
           <div class='static-flex-container'>
             <div class='static-flex-container-text'>
               <h1>8</h1>
-              <p>Contributions</p>
+              <p>Projects</p>
             </div>
             <div class='static-flex-container-text'>
               <h1>2.5</h1>
