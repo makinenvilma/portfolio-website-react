bdbfd3517ec953d5c1575a52e35a3010e8448c10
Vilma <vilmakinen@outlook.com>
2024-05-28 13:39:03 +0300

    Add: social media icons

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import styles from './Footer.module.css'; // Import the CSS module
+import styles from './Footer.module.css';
+import footer_linkedin_icon from '../../assets/icons/footer_linkedin_icon.png';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      {' '}
-      {/* Assigning footer class */}
+      <div className={styles['social-media-box']}>
+        <div className={styles['social-media-flex-container']}>
+          <div className={styles['info-flex-container-icon']}>
+            <img src={footer_linkedin_icon} alt='LinkedIn' />
+          </div>
+          <div className={styles['info-flex-container-icon']}>
+            <img src={footer_linkedin_icon} alt='LinkedIn' />
+          </div>
+          <div className={styles['info-flex-container-icon']}>
+            <img src={footer_linkedin_icon} alt='LinkedIn' />
+          </div>
+        </div>
+      </div>
       <p>&copy; 2024 Vilma Mäkinen. All rights reserved. </p>
     </footer>
   );
