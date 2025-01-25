e14360a1fef1485e5f3d403dcd60ac9573b455a2
makinenvilma <vilmakinen@outlook.com>
2025-01-25 17:11:42 +0200

    Add: about folder

---
+++ frontend/src/pages/about/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './about.css';
+
+const About = () => {
+  return (
+    <div>
+      <header></header>
+      <main>
+        <div class='about-me-box'>
+          <h1>UNDER CONSTRUCTION...</h1>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default About;
